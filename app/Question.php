2af48e664d41ae5a4d1f8b3404a4608e36b50917
neonemi <nemichand@neologicx.com>
2021-08-05 17:19:15 +0530

    add question module

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function questionsetting()
+    {
+        return $this->hasOne('App\QuestionsSetting', 'question_id', 'id')->with('domain', 'difflevel', 'age_group');
+    }
 }
