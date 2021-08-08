6399e79c3e0982e3d58f14cbef436b76f3287d56
neonemi <nemichand@neologicx.com>
2021-08-08 12:34:24 +0530

    add chnages for login api

--- app/User.php
+++ app/User.php
@@ -2,12 +2,11 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use Notifiable, HasApiTokens;
 
