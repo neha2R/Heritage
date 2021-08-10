7d6bffeee04486f156e29f8ece12225dbd31d0d5
neonemi <nemichand@neologicx.com>
2021-08-10 12:22:13 +0530

    chnages made for question api

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -423,7 +423,7 @@ class QuestionController extends Controller
         if (count($question_ids) < $speed->no_of_question) {
             $dis3 = $speed->no_of_question - count($question_ids);
             $question_id2 = QuestionsSetting::inRandomOrder()->whereIn('question_id', '!=', $question_ids)->limit($dis3)->pluck('question_id')->toArray();
-            $question_ids = array_merge($question_id2, $question_ids);
+            array_merge($question_id2, $question_ids);
         }
 
         // print_r($question_ids);exit;
