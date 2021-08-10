5a5f37c5c35ffcfba8455eb95dea4f20f7353150
neonemi <nemichand@neologicx.com>
2021-08-10 14:24:07 +0530

    chnages made

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -398,7 +398,7 @@ class QuestionController extends Controller
                 $question_id2 = QuestionsSetting::orWhere('difficulty_level_id', 2)->limit($dis2)->pluck('question_id')->toArray();
                 $question_ids = array_merge($question_id1, $question_id2);
 
-                dd($question_ids);
+                dd($question_id1, $question_id2);
                 // $question_ids = $question_ids->get()->toArray();
                 break;
             case "intermediate":
