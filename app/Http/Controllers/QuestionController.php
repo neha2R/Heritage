451d6cf792b27f64cecbe953ed8ac21362a76ae2
neonemi <nemichand@neologicx.com>
2021-08-13 13:00:29 +0530

    chnages made for table responsive issue in question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -466,7 +466,7 @@ class QuestionController extends Controller
 
         // print_r($question_ids);exit;
         shuffle($question_ids);
-        print_r($question_ids);exit;
+
         $data = [];
         $questions = Question::select('id', 'question', 'question_media', 'option1', 'option1_media', 'option2', 'option2_media', 'option3', 'option3_media', 'option4', 'option4_media', 'why_right', 'right_option', 'hint', 'question_media_type')->whereIn('id', $question_ids)->get();
         $data['quiz_type'] = $quiz_type->name;
