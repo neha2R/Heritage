af5e36a06abb5c889da99a28f1206bf3281126be
prince-aadil <prince.aadil@neologicx.com>
2021-09-23 11:06:18 +0530

    made changes in view of product

--- resources/views/product/list.blade.php
+++ resources/views/product/list.blade.php
@@ -363,7 +363,7 @@
          </div>
          <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Continue</button>
+           
             </form>
          </div>
          
