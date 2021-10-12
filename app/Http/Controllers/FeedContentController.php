9018b7d0289b90dd323f9e1208e671476d87a2c5
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2021-10-12 10:38:10 +0530

    made change for pagination

--- app/Http/Controllers/FeedContentController.php
+++ app/Http/Controllers/FeedContentController.php
@@ -711,9 +711,9 @@ return $request;
        
         // $feedContents2 = FeedContent::select('id','type','tags','title','description')->with('feedtype')->whereIn('feed_id',$feed_id)->whereIn('domain_id',$domain_id)->with(array('feed_media'=>function($query){$query->select('id','feed_content_id','title','description','external_link','video_link');}))->get(15);
         
-        $feedContents = $feedContents->where('id','>=',$request->feed_page_id)->take(5)->OrderBy('id', 'DESC')->get();
+        $feedContents = $feedContents->where('id','>=',$request->feed_page_id)->take(5)->OrderBy('id', 'DESC')->paginate(5);
         $data=[];
-        $last_page='';
+    //    $last_page='';
         $i=1;
         foreach($feedContents as $cont){
           $mydata['id'] = $cont->id; 
@@ -769,7 +769,7 @@ return $request;
           
           $mydata['media'] = $imgdata; 
           $data[]=$mydata;
-          $last_page = $cont->id;
+         // $last_page = $cont->id;
           $i++;
         }
        
