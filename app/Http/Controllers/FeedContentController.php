f6ceb09b928e39db673d8512c69aacc55c2b52e8
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2021-10-12 10:50:01 +0530

    made changes

--- app/Http/Controllers/FeedContentController.php
+++ app/Http/Controllers/FeedContentController.php
@@ -711,9 +711,9 @@ return $request;
        
         // $feedContents2 = FeedContent::select('id','type','tags','title','description')->with('feedtype')->whereIn('feed_id',$feed_id)->whereIn('domain_id',$domain_id)->with(array('feed_media'=>function($query){$query->select('id','feed_content_id','title','description','external_link','video_link');}))->get(15);
         
-        $feedContents = $feedContents->where('id','>=',$request->feed_page_id)->OrderBy('id', 'DESC')->paginate(2);
+        $feedContents = $feedContents->where('id','>=',$request->feed_page_id)->take(5)->OrderBy('id', 'DESC')->get();
         $data=[];
-    //    $last_page='';
+        $last_page='';
         $i=1;
         foreach($feedContents as $cont){
           $mydata['id'] = $cont->id; 
@@ -769,14 +769,14 @@ return $request;
           
           $mydata['media'] = $imgdata; 
           $data[]=$mydata;
-         // $last_page = $cont->id;
+          $last_page = $cont->id;
           $i++;
         }
        
         if(empty($feedContents)){
             return response()->json(['status' => 200, 'message' => 'Feed not available', 'data' => '']);
         }
-        return response()->json(['status' => 200, 'message' => 'Domain data', 'last_id'=>$feedContents->links(),'data' => $data]);
+        return response()->json(['status' => 200, 'message' => 'Domain data', 'last_id'=>$last_page,'data' => $data]);
 
     }
 
