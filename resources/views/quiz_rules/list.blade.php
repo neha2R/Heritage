5faf79ebe64c679d00828c4849b371ef022631d5
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2021-10-13 15:41:00 +0530

    changes made for quiz percentage

--- resources/views/quiz_rules/list.blade.php
+++ resources/views/quiz_rules/list.blade.php
@@ -261,8 +261,9 @@
                   <input type="text" maxlength="100" class="@error('que_navigation') is-invalid @enderror form-control" name="que_navigation" placeholder="Question Navigation" required value="{{$rule->que_navigation}}">
                </div>
                <div class="form-group moreone">
+                  {{dd($rule->more)}}
                   @if($rule->more != null)
-                  @php $myrule = json_decode($rule->more) @endphp
+                  @php $myrule = json_decode($rule->more); @endphp
                   @foreach($myrule as $Rule)
                   <div class="row box">
                      <div class="form-group col-md-10">
