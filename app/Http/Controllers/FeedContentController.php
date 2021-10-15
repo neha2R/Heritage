984d95a36be6ef4bf8b3492160b32ad7b422d8ba
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2021-10-15 18:19:17 +0530

    changes made

--- app/Http/Controllers/FeedContentController.php
+++ app/Http/Controllers/FeedContentController.php
@@ -749,7 +749,7 @@ return $request;
             $place =null;
               }
           $mydata['placeholder_image'] =$place;  
-          $savefeeds = SaveFeed::where('feed_contents_id',$cont->id)->pluck('feed_contents_id');
+          $savefeeds = SaveFeed::where('feed_contents_id',$cont->id)->get()->toArray();
           $mydata['savepost'] = count($savefeeds); 
                 if(isset($cont->savefeed)){
                     $save = 1;
