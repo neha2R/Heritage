97acad073cd10598bb2ba2eb9fcb77504edfaf85
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2021-10-15 17:53:50 +0530

    changes made

--- app/Http/Controllers/FeedContentController.php
+++ app/Http/Controllers/FeedContentController.php
@@ -999,7 +999,12 @@ return $request;
             //   }
               $mydata['is_saved'] = 1; 
               $mydata['share'] = $this->sharepath($cont->id); 
-              $mydata['media_type'] = $cont->feed_media_single->feed_attachments_single->media_type; 
+              if(isset($cont->feed_media_single->feed_attachments_single)){
+                  $mediatype = $cont->feed_media_single->feed_attachments_single->media_type;
+              }else{
+                $mediatype = '';  
+              }
+              $mydata['media_type'] = $mediatype; 
               $imagename=[];
               foreach($cont->feed_media_single->feed_attachments_name as $image){
                  
