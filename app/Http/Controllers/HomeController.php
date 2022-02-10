497d82b85d61b9316c91c3136db2410d74389633
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2022-02-10 14:53:22 +0530

    dashboard api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,7 +36,8 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function dashboard(Request $request){
+    public function dashboard(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'user_id' => 'required',
         ]);
@@ -45,48 +46,41 @@ class HomeController extends Controller
             return response()->json(['status' => 422, 'data' => '', 'message' => $validator->errors()]);
         }
 
-       $user =  User::find($request->user_id);
-       if(isset($user)){
-       $contacts = Contact::where('friend_two',$request->user_id)->where('status','0')->get();
-       $duals = Challange::where('to_user_id',$request->user_id)->where('status','0')->get();
-   $data=[];
-   $response=[];
-   foreach($contacts as $contact)
-   {
-        $user = User::where('id', $contact->friend_two)->first();
-        $data['name'] = $user->name;
-        if (isset($user->profile_image)) {
-            $data['image'] = url('/images') . '/' . $user->profile_image;
+        $user =  User::find($request->user_id);
+        if (isset($user)) {
+            $contacts = Contact::where('friend_two', $request->user_id)->where('status', '0')->get();
+            $duals = Challange::where('to_user_id', $request->user_id)->where('status', '0')->get();
+            $data = [];
+            $response = [];
+            foreach ($contacts as $contact) {
+                $user = User::where('id', $contact->friend_two)->first();
+                $data['id'] = $contact->id;
+                $data['name'] = $user->name;
+
+                if (isset($user->profile_image)) {
+                    $data['image'] = url('/images') . '/' . $user->profile_image;
+                } else {
+                    $data['image'] = '';
+                }
+                $data['link'] = $user->refrence_code;
+                $response['contact'][] = $data;
+            }
+
+            foreach ($duals as $dual) {
+                $user = User::where('id', $dual->from_user_id)->first();
+                $data['name'] = $user->name;
+                if (isset($user->profile_image)) {
+                    $data['image'] = url('/images') . '/' . $user->profile_image;
+                } else {
+                    $data['image'] = '';
+                }
+                $data['link'] = Attempt::where('id', $dual->attempt_id)->first()->link;
+                $data['dual_id'] = $dual->attempt_id;
+                $response['dual'][] = $data;
+            }
+            return response()->json(['status' => 200, 'data' => $response, 'message' => 'Data']);
         } else {
-            $data['image'] = '';
+            return response()->json(['status' => 201, 'data' => [], 'message' => 'User not found..']);
         }
-        $data['link'] = $user->refrence_code;
-        $data['type'] = 'contact';
-        $response[] = $data;
-
-   }
-
-   foreach($duals as $dual)
-   {
-        $user = User::where('id', $dual->from_user_id)->first();
-        $data['name'] = $user->name;
-        if (isset($user->profile_image)) {
-            $data['image'] = url('/images') . '/' . $user->profile_image;
-        } else {
-            $data['image'] = '';
-        }
-        $data['link'] = Attempt::where('id',$dual->attempt_id)->first()->link;
-        $data['type'] = 'dual';
-        $data['dual_id'] = $dual->attempt_id;
-        $response[] = $data;
-
-   }
-   return response()->json(['status' => 200, 'data' => $response, 'message' => 'Data']);
-
-    }
-    else{
-        return response()->json(['status' => 201, 'data' => [], 'message' => 'User not found..']);
-
-       }
     }
 }
