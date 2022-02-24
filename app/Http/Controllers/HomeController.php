d6bbf37d6898189448b407ee998c9c1fe2b88b6b
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2022-02-24 11:38:52 +0530

    add link details api in home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -136,14 +136,67 @@ class HomeController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['status' => 422, 'data' => '', 'message' => $validator->errors()]);
+            return response()->json(['status' => 422, 'data' => [], 'message' => $validator->errors()]);
         }
           
         $string = explode('/',$request->link);
         $string = explode('#',$string['1']);
-
-        dd($string);
+        $response = [];
         
+        if($string['0']=='duel'){
+            $data = Attempt::where('link', $request->link)->first();
+            if (empty($data)) {
+                return response()->json(['status' => 204, 'message' => 'Sorry! Link has been expired. or not found']);
+            }
+            $domain =  QuizDomain::where('attempts_id', $data->id)->first()->domain_id;
+
+            $domains = explode(',', $domain);
+
+            
+            $response['dual_id'] = $data->id;
+            $response['domain'] = implode(',', Domain::whereIn('id', $domains)->pluck('name')->toArray());
+            $response['quiz_speed'] = ucwords(strtolower($data->quiz_speed->name));
+            $response['difficulty'] = ucwords(strtolower($data->difficulty->name));
+            $response['link'] = $data->link;
+            $response['created_date'] = date('d-M-Y', strtotime($data->created_at));
+            // $response['type'] = 'dual';
+            return response()->json(['status' => 200, 'data' => $response, 'type' => 'duel', 'message' => 'Dual data']);
+
+        }
+        else if ($string['0'] == 'invite') {
+            
+            $user = User::where('refrence_code', $string[1])->first();
+           
+            if (!$user) {
+                return response()->json(['status' => 201, 'data' => [], 'message' => 'Link is not valid']);
+            }
+            $oldFriend = Contact::where('friend_one', $user->id)->where('friend_two', $request->user_id)->first();
+            if (isset($oldFriend) && $oldFriend->status == '1' ) 
+            {
+                return response()->json(['status' => 201, 'data' => [], 'message' => 'Friend already added']);
+            }
+            $data['id'] = $user->id;
+            $data['name'] = $user->name;
+
+            if (isset($user->profile_image)) {
+                $data['image'] = url('/storage') . '/' . $user->profile_image;
+            } else {
+                $data['image'] = '';
+            }
+           
+            $data['link'] = $request->link;
+            
+            $response = $data;
+            return response()->json(['status' => 200, 'data' => $response,'type'=> 'invite', 'message' => 'Contact data']);
+
+        }
+
+        else{
+            return response()->json(['status' => 201, 'data' => [], 'message' => 'Not a valid link']);
+ 
+        }
+        
+
 
     }
 }
