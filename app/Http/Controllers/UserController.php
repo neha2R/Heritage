066bd14d53c85707520e2100222edc3f60309dea
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2022-03-29 11:23:23 +0530

    login chnages

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,9 +38,11 @@ class UserController extends Controller
 
         if ($request->is_social == 1) {
             $user = User::where('email', '=', request('email'))->first();
-
+            if (!$user) {
+            return response()->json(['status' => 202, 'message' => "Email or password is invalid.", 'data' => '']);
+            }
             if ($user->password != null) {
-                return response()->json(['status' => 202, 'message' => "Email is invalid.", 'data' => ''], 200);
+                return response()->json(['status' => 202, 'message' => "Email is invalid.", 'data' => '']);
             }
 
             if (Auth::loginUsingId($user->id)) {
