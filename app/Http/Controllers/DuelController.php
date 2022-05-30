0e1a319c8f37e14170b54fb2accc7eace9b35e70
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2022-05-30 16:59:41 +0530

    check league rank

--- app/Http/Controllers/DuelController.php
+++ app/Http/Controllers/DuelController.php
@@ -745,7 +745,7 @@ class DuelController extends Controller
                
                 
                     $rank++;
-                
+                $olddata = $rankdata;
 
                
             }
