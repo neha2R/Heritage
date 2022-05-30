2af781eb90005e87f7121e0e51c99a5eaeec566d
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2022-05-30 11:30:03 +0530

    Duel result

--- app/Http/Controllers/DuelController.php
+++ app/Http/Controllers/DuelController.php
@@ -721,7 +721,7 @@ class DuelController extends Controller
                 }
             }
         }
- dd($rankdata);
+
         if ($rankdata) {
             $rank = 1;
             $myrank = 0;
