c759b8ebdcf4e0724f80cf12057574f17eae7747
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2022-05-30 18:39:35 +0530

    check league rank

--- app/Http/Controllers/FeedContentController.php
+++ app/Http/Controllers/FeedContentController.php
@@ -822,7 +822,7 @@ class FeedContentController extends Controller
             $savefeeds = SaveFeed::where('feed_contents_id', $cont->id)->pluck('feed_contents_id');
             $mydata['savepost'] = count($savefeeds);
             if (isset($cont->savefeed)) {
-                        if ($cont->savefeed->user_id == $request->user->id){
+                        if ($cont->savefeed->user_id == $request->user_id){
                 $save = 1;
                         } else{
                             $save = 0;
