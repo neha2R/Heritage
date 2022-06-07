2094db287421dd2bf1565960816fd59c77774193
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2022-06-07 15:45:58 +0530

    dual result if any user leave

--- app/Http/Controllers/DuelController.php
+++ app/Http/Controllers/DuelController.php
@@ -440,6 +440,7 @@ class DuelController extends Controller
             } else {
                 $data2 =  Attempt::where('parent_id', $request->dual_id)->first();
             }
+            dd($data2);
             if ($data->user_id == $request->user_id) {
                 $user_data = $data;
                 $otheruser_data = $data2;
