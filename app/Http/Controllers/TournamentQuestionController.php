f4cfc4ffa0a0e20a5dff5d1206097230250a9f0a
Virendra Singh <virendra.singh.shekhawat@neologicx.com>
2022-06-21 18:24:18 +0530

    tournament question setting

--- app/Http/Controllers/TournamentQuestionController.php
+++ app/Http/Controllers/TournamentQuestionController.php
@@ -51,9 +51,46 @@ class TournamentQuestionController extends Controller
        } else {
            $mydata=[];
         $questions_ids = json_decode($tourQuestions->questions);
-                foreach($questions_ids as $ids){
-                    $questions = Question::select('id', 'question', 'question_media', 'option1', 'option1_media', 'option2', 'option2_media', 'option3', 'option3_media', 'option4', 'option4_media', 'why_right', 'right_option', 'hint', 'question_media_type', 'type As question_media_type', 'ques_type')->where('id', $ids)->first()->toArray(); 
-                    $mydta[] = $questions; 
+            $questions = Question::select('id', 'question', 'question_media', 'option1', 'option1_media', 'option2', 'option2_media', 'option3', 'option3_media', 'option4', 'option4_media', 'why_right', 'right_option', 'hint', 'attachment_details', 'type', 'ques_type')->where('id', $ids)->first()->toArray(); 
+
+                foreach($questions as $que){
+                // $questions = Question::select('id', 'question', 'question_media', 'option1', 'option1_media', 'option2', 'option2_media', 'option3', 'option3_media', 'option4', 'option4_media', 'why_right', 'right_option', 'hint', 'question_media_type', 'type As question_media_type', 'ques_type')->where('id', $ids)->first()->toArray(); 
+                $quesdata['id'] = $que->id;
+                $quesdata['question'] = $que->question;
+                if ($que->question_media != null) {
+                    $quesdata['question_media'] = url('/storage') . '/' . $que->question_media;
+                    if ($que->type == '1') {
+                        $detail = (array) json_decode($que->attachment_details, true);
+                        // $h = 0;
+                        // $w = 1;
+                        // dd($detail);
+                        // $quesdata['width']  =$detail['0'];
+                        // $quesdata['height']  =$detail['1'];
+                        $quesdata['width']  = 800;
+                        $quesdata['height']  = 800;
+                        //  $quesdata['media_data']  =$detail;
+                    }
+                } else {
+                    $quesdata['question_media'] = '';
+                    $quesdata['width']  = '';
+                    $quesdata['height']  = '';
+                }
+                $quesdata['option1'] = $que->option1;
+                $quesdata['option1_media'] = $que->option1_media;
+                $quesdata['option2'] = $que->option2;
+                $quesdata['option2_media'] = $que->option2_media;
+                $quesdata['option3'] = $que->option3;
+                $quesdata['option3_media'] = $que->option3_media;
+                $quesdata['option4'] = $que->option4;
+                $quesdata['option4_media'] = $que->option4_media;
+                $quesdata['right_option'] = $que->right_option;
+                $quesdata['hint'] = $que->hint;
+                $quesdata['question_media_type'] = $que->type;
+                $quesdata['why_right'] = $que->why_right;
+                $quesdata['type'] = $que->type;
+                $quesdata['ques_type'] = $que->ques_type;
+               
+                    $mydta[] = $quesdata; 
                 }
                 $data['question'] = $mydta;
                 $data['whole_quiz_time'] = '1';
